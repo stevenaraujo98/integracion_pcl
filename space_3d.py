63789921a22edad96bbf6d32b5c5d873ac6b8b3b
Steven Araujo <steven.araujo@hotmail.com>
2024-08-28 15:13:32 -0500

    correccion get_connection_points

--- space_3d.py
+++ space_3d.py
@@ -255,9 +255,9 @@ def get_connection_points(list_centroides, name_common, step_frames, centroide, 
     centroide_tmp = centroide[[0, 2]]
     avg_normal_tmp = avg_normal[[0, 2]]
     character, confianza = "", 0
-    hull_simplices = []
     val_dif_mayor_menor = 0
     list_pos_extremo = []
+    list_union_centroides = []
 
     if len(puntos) <= 1:
         return [], character, confianza
@@ -274,35 +274,34 @@ def get_connection_points(list_centroides, name_common, step_frames, centroide, 
         pt_menor = puntos_sorted[0, 1]
         pt_mayor = puntos_sorted[-1, 1]
         val_dif_mayor_menor = pt_mayor - pt_menor
-        if val_dif_mayor_menor <= 20: 
+        if val_dif_mayor_menor <= 30: 
             for i in range(len(puntos_sorted) - 1):
                 index = np.where(puntos == puntos_sorted[i])[0][0]
                 index_2 = np.where(puntos == puntos_sorted[i+1])[0][0]
-                hull_simplices.append([index, index_2])
-            list_pos_extremo.append(hull_simplices[0])
-            list_pos_extremo.append(hull_simplices[-1])
+                list_union_centroides.append([index, index_2])
+            list_pos_extremo.append([np.where(puntos == puntos_sorted[0])[0][0], np.where(puntos == puntos_sorted[-1])[0][0]])
         else:
             # Calcular la envolvente convexa
             hull = ConvexHull(puntos)
-            hull_simplices = hull.simplices
 
-        # Calcular el punto final del vector
-        vector_end = centroide_tmp + avg_normal_tmp * 1000  # Escalar para que sea largo
-        list_union_centroides = []
+            # Calcular el punto final del vector
+            vector_end = centroide_tmp + avg_normal_tmp * 1000  # Escalar para que sea largo
 
-        for simplex in hull_simplices:
-            p1, p2 = puntos[simplex]
-            interseccion = line_intersection(p1, p2, centroide_tmp, vector_end)
-            if interseccion is None:
-                list_union_centroides.append(simplex)
-                p1_3D, p2_3D = list_centroides[simplex]
+            for simplex in hull.simplices:
+                p1, p2 = puntos[simplex]
+                interseccion = line_intersection(p1, p2, centroide_tmp, vector_end)
+                if interseccion is None:
+                    list_union_centroides.append(simplex)
+                else:
+                    list_pos_extremo.append(simplex)
+
+        for simplex in list_union_centroides:
+            p1_3D, p2_3D = list_centroides[simplex]
                 
-                if ax:
-                    ax.plot([p1_3D[0], p2_3D[0]],
-                            [p1_3D[1], p2_3D[1]],
-                            [p1_3D[2], p2_3D[2]], color='orange')
-            else:
-                list_pos_extremo.append(simplex)
+            if ax:
+                ax.plot([p1_3D[0], p2_3D[0]],
+                        [p1_3D[1], p2_3D[1]],
+                        [p1_3D[2], p2_3D[2]], color='orange')
 
     character, confianza = get_img_shape_meet_prev_sort(list_union_centroides, puntos, name_common, step_frames, centroide, list_pos_extremo)
 
